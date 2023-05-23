509594ea42f603f433e45ce5073eb5ebc65eada2
Abigail Poropatich <92827108+aporopa@users.noreply.github.com>
2023-05-23 11:48:23 -0400

    Add files via upload

---
+++ Prog1/my_loop.m
@@ -0,0 +1,42 @@
+comment( ' Abigail Poropatich
+    CPSC 2310 001
+    Lab 1: Accumulator Machine
+    23 May 2023 ')
+comment(' set aside memory and initialize variables ')
+
+word(sum,0)  
+word(i,0)
+word(zero,0)
+word(one,1)
+word(ten,10)
+
+label(start)
+    comment( ' sum = 0 ' )
+    load(zero)
+    store(sum)
+
+    comment( ' i = 1 ' )
+    load(one)
+    store(i)
+
+label(loop) 
+    comment(' sum = sum = i ' )
+    load(sum) 
+	add(i)
+	store(sum) 
+
+    comment(' i++ ' )
+    load(i)
+    add(one)
+    store(i)
+    
+    comment( ' Branch back to loop if ACC <= 10 ' )
+    ble0(loop)
+
+label(done)
+
+    comment(' program operations ')
+    print(sum)
+    halt
+
+    start
---
+++ Prog1/prog1.m
@@ -0,0 +1,49 @@
+comment(`Abigail Poropatich')
+comment(`CPSC 2310 - 001')
+comment(`Binary to Decimal conversion')
+comment(`26 May 2023')
+
+
+comment(`Variable definitions')
+word(N, 1101)
+word(decimal, 0)    
+word(power, 1)      
+word(two, 2)
+word(done, 0) 
+word(ten, 10)
+word(neg, -1)    
+
+label(start)
+    load(N)
+    ble0(done)
+
+label(loop)
+                    comment(`Performing modulus division')
+    div(ten)
+    mul(ten)
+                    comment(`Extracting the first value and multiplying to positive')
+    sub(N)
+    mul(neg)
+    mul(power)
+                    comment(`Adding and storing the decimal value')
+    add(decimal)
+    store(decimal)
+
+                    comment(`Updating the power')
+    load(power)
+    mul(two)
+    store(power)
+                    
+                    comment(`Updating N for the next iteration')
+    load(N)
+    div(ten)
+    store(N)
+
+                    comment(`N > 0')
+    bgt0(loop)
+
+label(done)
+    print(decimal)
+    halt
+
+    end(start)
