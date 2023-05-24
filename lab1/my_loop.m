710bb8e6be761d8341135e4254ccdd2a4d424e28
Abigail Poropatich <92827108+aporopa@users.noreply.github.com>
2023-05-24 00:11:34 -0400

    Update my_loop.m

--- lab1/my_loop.m
+++ lab1/my_loop.m
@@ -1,41 +1,42 @@
-comment( ' Abigail Poropatich
+comment('Abigail Poropatich
     CPSC 2310 001
     Lab 1: Accumulator Machine
-    23 May 2023 ')
-comment(' set aside memory and initialize variables ')
+    23 May 2023')
+comment('set aside memory and initialize variables')
 
 word(sum,0)  
-word(i,0)
+word(i,1)
 word(zero,0)
 word(one,1)
 word(ten,10)
 
 label(start)
-    comment( ' sum = 0 ' )
+    comment('sum = 0')
     load(zero)
     store(sum)
 
-    comment( ' i = 1 ' )
+    comment('i = 1')
     load(one)
     store(i)
 
 label(loop) 
-    comment(' sum = sum = i ' )
+    comment('sum = sum = i')
     load(sum) 
 	add(i)
 	store(sum) 
 
-    comment(' i++ ' )
+    comment('i++')
     load(i)
     add(one)
     store(i)
     
-    comment( ' Branch back to loop if ACC <= 10 ' )
+    comment('Branch back to loop if ACC <= 10')
+    sub(ten)
     ble0(loop)
 
 label(done)
 
-    comment(' program operations ')
+    comment('program operations')
     print(sum)
     halt
 
